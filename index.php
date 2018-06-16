1ad0ce538bac2d409b0b4d48f02e6ad14a24a925
Tusdasa翼 <tusdasa@tusdasa.net>
2018-06-16 20:16:27 +0800

    设置正确的H5

--- index.php
+++ index.php
@@ -1,55 +1,84 @@
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
 <title></title>
+<meta charset="utf-8">
 <style type="text/css">
-.main{
-width: 400px;
-height: 300px;
-margin: 200px auto;
+* {
+	padding: 0px;
+	margin: 0px;
 }
-.link{
-width: 400px;
-height: 100px;
-margin: auto;
+
+.main {
+	width: 400px;
+	height: 300px;
+	margin: 200px auto;
+}
+
+.link {
+	width: 400px;
+	height: 100px;
+	margin: auto;
+}
+
+header {
+	width: 100%;
+	height: 200px;
+}
+
+header nav {
+	width: 80%;
+	height: 190px;
+	margin: auto;
+}
+
+nav p {
+	font-size: 50px;
+	text-align: center;
+	line-height: 190px;
 }
 </style>
 </head>
 <body>
-
-<div class="main">
-<form action="#" method="post">
-<input style="margin: auto; width: 350px" type="password" title="Please input you pass" name="flag">
-<input type="submit">
-<p style="text-align: center;">pass: 123456<p>
-</form>
-
-
+	<header>
+		<nav>
+			<p>Welcome Web Application</p>
+		</nav>
+	</header>
+	<section>
+		<div class="main">
+			<form action="#" method="post">
+				<input style="margin: auto; width: 350px" type="password"
+					title="Please input you pass" name="flag"> <input type="submit">
+				<p style="text-align: center;">pass: 123456<p>
+			</form>
 <?php
-if(isset($_POST["flag"])){
-    $flag=$_POST["flag"];
-    if (strcmp($flag, "123456")==0){
+if (isset($_POST["flag"])) {
+    $flag = $_POST["flag"];
+    if (strcmp($flag, "123456") == 0) {
         session_start();
-        $_SESSION["user"]="admin";
-    }else {
-        echo "<p style=\"text-align: center;\">"."wrong password"."<p>";
+        $_SESSION["user"] = "admin";
+    } else {
+        echo "<p style=\"text-align: center;\">" . "wrong password" . "<p>";
     }
 }
 ?>
 </div>
+<footer>
 <div class="link">
-<table style="align: center;">
-<tr>
-<td style="width: 100px"><a href="/test/XSS" target="_blank">XSS</a>
-</td>
-<td style="width: 100px"><a href="/test/CSRF" target="_blank">CSRF</a>
-</td>
-<td style="width: 100px"><a href="/test/upload" target="_blank">File Upload</a>
-</td>
-<td style="width: 100px"><a href="#" target="_blank">-_-</a>
-</td>
-</tr>
-</table>
-</div>
+		<table style="align: center;">
+			<tr>
+				<td style="width: 100px"><a href="/test/XSS" target="_blank">XSS</a>
+				</td>
+				<td style="width: 100px"><a href="/test/CSRF" target="_blank">CSRF</a>
+				</td>
+				<td style="width: 100px"><a href="/test/upload" target="_blank">File
+						Upload</a></td>
+				<td style="width: 100px"><a href="#" target="_blank">-_-</a></td>
+			</tr>
+		</table>
+	</div>
+</footer>
+	</section>
 </body>
 </html>
